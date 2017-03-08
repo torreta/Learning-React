accb67ee45ebc0812609c7549d7f26ac557f725e
Luis Campos <lcampos@strix.com.ve>
2017-03-08 12:55:24 -0400

    adding listView and making a simple datasource for it, since is required as a component, creating private method renderPersonRow with underscore standart

--- index.ios.js
+++ index.ios.js
@@ -5,27 +5,65 @@
  */
 
 import React, { Component } from 'react';
-import {AppRegistry, StyleSheet, Text, View } from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, ListView } from 'react-native';
 
 import ViewContainer from './app/components/ViewContainer'
 import StatusBarBackground from './app/components/StatusBarBackground'
 
 import message from './temp'
 
+const people = [
+                {id: "1", firstName: "Luis", lastName: "Campos", roomNumber: 30},
+                {id: "2", firstName: "Wilghe", lastName: "Alzualde", roomNumber: 22},
+                {id: "3", firstName: "Anibal", lastName: "Abdulkhalek", roomNumber: 6}
+              ]
+
+
 export default class playapp extends Component {
+
+  constructor(props){
+    super(props)
+    var ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 != r2})
+
+    this.state = {
+      peopleDataSource: ds.cloneWithRows(people)
+    }
+
+  }
+
+
   render() {
     return (
 
       <ViewContainer>
         <StatusBarBackground style = {{backgroundColor: "skyblue" }} />
+
         <Text style = {{backgroundColor: "coral"}}>
           you see? works
         </Text>
+
+        <ListView style = {{marginTop: 100}}
+          dataSource = {this.state.peopleDataSource}
+          renderRow = {(person) => { return this._renderPersonRow(person)}}>
+
+
+        </ListView>
       </ViewContainer>
 
 
     );
   }
+
+  _renderPersonRow(person){
+    return(
+      <View style={styles.personRow}>
+        <Text style = {styles.personName}>
+          {person.firstName}
+        </Text>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -35,15 +73,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  personRow: {
+
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  personName: {
+
   },
 });
 
