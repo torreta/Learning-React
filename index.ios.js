3f6a352d59a122f1385aec94a9326b44d8d8da26
Luis Campos <lcampos@strix.com.ve>
2017-03-10 11:36:35 -0400

    implemented tabbar navigation, ant its onPress capacity per TabBarItem

--- index.ios.js
+++ index.ios.js
@@ -5,12 +5,40 @@ import AppNavigator from './app/navigation/AppNavigator'
 
 
 export default class playapp extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedTab: "tab1_index"
+    }
+  }
+
 
   render() {
     return (
-      <AppNavigator
-        initialRoute ={{ident: "PeopleIndex"}}
-      />
+      <TabBarIOS selectedTab={this.state.selectedTab}>
+
+        <TabBarIOS.Item
+          selected = {this.state.selectedTab === "tab1_index"}
+          title = {`TAB 1 - INDEX`}
+          onPress={() => this.setState({selectedTab: "tab1_index"})}
+        >
+          <AppNavigator
+          initialRoute ={{ident: "PeopleIndex"}} />
+
+
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          selected = {this.state.selectedTab === "tab2_show"}
+          title = {`TAB 2 - PERSON`}
+          onPress={() => this.setState({selectedTab: "tab2_show"})}
+        >
+          <AppNavigator
+          initialRoute ={{ident: "PersonShow", person: {id: "1", firstName: "Luis", lastName: "Campos", roomNumber: 30}}} />
+
+        </TabBarIOS.Item>
+
+      </TabBarIOS>
     )
   }
 
