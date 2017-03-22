4d2ee8dc65a9b3e5425cc8ea16a035e3e817546a
Luis Campos <lcampos@strix.com.ve>
2017-03-22 11:28:14 -0400

    added the Picker and 3 values for notifications

--- app/screens/NotificationScreen.js
+++ app/screens/NotificationScreen.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native'
-
-import ViewContainer from '../components/ViewContainer'
+import { View, StyleSheet, Text, Picker } from 'react-native'
 import StatusBarBackground from '../components/StatusBarBackground'
 
 class NotificationScreen extends Component {
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            seconds: 5,
+        }
+    }
+
+
     render(){
         return (
 
-          <ViewContainer>
+          <View style = {styles.container} >
             <StatusBarBackground style = {{backgroundColor: "skyblue" }} />
 
 
-                <Text style={styles.Text}>
-                    nada notification
+                <Text style={styles.mensaje}>
+                   Tiempo para la Notificación:
                 </Text>
 
+                <Picker
+                    style = {styles.picker}
+                    selectedValue = {this.state.seconds}
+                    onValueChange = {(seconds) => this.setState({seconds})}
+                >
 
-          </ViewContainer>
+                    <Picker.Item label="5" value={5} />
+                    <Picker.Item label="10" value={10} />
+                    <Picker.Item label="15" value={15} />
+
+                </Picker>
+
+
+          </View>
 
         )
     }
 }
 
 const styles = StyleSheet.create({
-  Text: {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  mensaje: {
     height: 20,
-  }
+  },
+  picker: {
+    width: 100,
+  },
 
 })
 
